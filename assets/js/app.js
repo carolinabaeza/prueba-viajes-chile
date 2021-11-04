6598b87ad7454ffbb6b4c018a140fcb23d9a81d4
Carolina Baeza <carolina.baeza.@destacados.cl>
2021-11-04 04:08:58 -0300

    segundo compromiso viajes chile

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  });
+
+
+$(".favoritos").click (function () {
+    alert("Añadido a favoritos") 
+});
+
+$("#accion-tarjeta-1").click (function () {
+    $(".texto-tarjeta-1").toggle ();
+    });
+    
+    $("#accion-tarjeta-2").click (function () {
+        $(".texto-tarjeta-2").toggle ();
+        });
+    
+        $("#accion-tarjeta-3").click (function () {
+            $(".texto-tarjeta-3").toggle ();
+            });
+
+            $("#accion-tarjeta-4").click (function () {
+                $(".texto-tarjeta-4").toggle ();
+                });
+
+
+
+
+
+                $(function () {
+                    $(document).scroll(function () {
+                      var $nav = $(".fixed-top");
+                      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+                    });
+                  });
