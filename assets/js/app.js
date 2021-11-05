0b5d6c7a48818bf1b163dd467fe3327917c89105
Carolina Baeza <carolina.baeza.@destacados.cl>
2021-11-04 22:33:03 -0300

    revision y correccion de textos clases funciones y html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,44 @@
+//tooltip
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   });
 
-
+//para añadir a favoritos
 $(".favoritos").click (function () {
     alert("Añadido a favoritos") 
 });
 
+//esconder aparecer info tarjeta
 $("#accion-tarjeta-1").click (function () {
     $(".texto-tarjeta-1").toggle ();
-    });
+});
     
-    $("#accion-tarjeta-2").click (function () {
-        $(".texto-tarjeta-2").toggle ();
-        });
+$("#accion-tarjeta-2").click (function () {
+    $(".texto-tarjeta-2").toggle ();
+});
     
-        $("#accion-tarjeta-3").click (function () {
-            $(".texto-tarjeta-3").toggle ();
-            });
+ $("#accion-tarjeta-3").click (function () {
+    $(".texto-tarjeta-3").toggle ();
+});
 
-            $("#accion-tarjeta-4").click (function () {
+$("#accion-tarjeta-4").click (function () {
                 $(".texto-tarjeta-4").toggle ();
-                });
+});
 
 
+//funcion menu scroll
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
 
 
-
-                $(function () {
-                    $(document).scroll(function () {
-                      var $nav = $(".fixed-top");
-                      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-                    });
-                  });
+//funcion texto amarillo
+$(".texto-amarillo").dblclick(function () {
+    $(this).css("color","#ffff00")
+    });
 
 
                   
